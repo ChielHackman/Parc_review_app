70a81fe74a80576f66779f6c8c4639715817e571
Chiel Hackman <chielhackman@gmail.com>
2016-02-28 18:50:31 +0100

    Migrate and seed the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+parc1 = Parc.create({ name: "De Eemhof", description: "Eldorado vol avontuur en watersport, met een Mediterraans tintje.", city: "Slingerweg 1 Zeewolde"})
+parc2 = Parc.create({ name: "Parc Zandvoort", description: "Uitwaaien aan het Noordzeestrand of voor even toerist in de grote stad.", city: "Vondellaan 60 Zandvoort"})
+parc3 = Parc.create({ name: "Het Meerdal", description: "Het paradijs waar kinderdromen uitkomen.", city: "Laagheideweg 11 America"})
+parc4 = Parc.create({ name: "Parc Sandur", description: "Rust en ruimte aan een glinsterend meer.", city: "Sandurdreef 5 Emmen"})
