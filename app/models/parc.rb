a0e8451ba7c9ccc54c7406aa8768504afb31faf1
Chiel Hackman <chielhackman@gmail.com>
2016-03-02 12:56:01 +0100

    Changed parc model

--- app/models/parc.rb
+++ app/models/parc.rb
@@ -1,15 +1,3 @@
 class Parc < ActiveRecord::Base
   has_many :reviews
-
-  def has_reviews?
-    self.reviews.present?
-  end
-
-  def average_rating
-    if has_reviews?
-      return self.reviews.average(:rating).round
-    else
-      return 0
-    end
-  end
 end
