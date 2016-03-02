5ac2ee365feda5a2038529fa283f52de5ccaedcc
Chiel Hackman <chielhackman@gmail.com>
2016-03-02 21:58:57 +0100

    Average rating

--- app/models/parc.rb
+++ app/models/parc.rb
@@ -2,14 +2,14 @@ class Parc < ActiveRecord::Base
   has_many :reviews
 
   def has_reviews?
-  self.reviews.present?
-  end
-
+     self.reviews.present?
+   end
+   
   def average_rating
-    if has_reviews?
-      return self.reviews.average
-    else
-      return 0
-    end
+      if self.reviews.present?
+        return self.reviews.average(:rating).round
+      else
+        return  0
+      end
   end
 end
