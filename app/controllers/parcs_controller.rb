8f0f2a4772ae8f3cd39dcf40a37a3199423009fc
Chiel Hackman <chielhackman@gmail.com>
2016-03-02 21:21:13 +0100

    Again

--- app/controllers/parcs_controller.rb
+++ app/controllers/parcs_controller.rb
@@ -5,7 +5,7 @@ class ParcsController < ApplicationController
         count: Parc.count,
         page: 0
       },
-      parcs: parc.as_json({:include => :reviews, :methods => :average_rating})
+      parcs: parc.json({:include => :reviews, :methods => :average_rating})
     }
   end
 
