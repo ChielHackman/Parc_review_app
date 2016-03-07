5af8256b30eff2377275d913ba499164f26fdda2
Chiel Hackman <chielhackman@gmail.com>
2016-03-07 10:07:13 +0100

    No acces

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,7 @@
 class ParcReviewApp::Application
   config.middleware.insert_before 0,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
     allow do
-      origins 'localhost:3000',
-      'http://parcreviewapp.bitballoon.com/'
+      origins 'localhost:3000'
 
       resource '/cors',
         headers: :any,
--- spec/routing/parcs_routing_spec.rb
+++ spec/routing/parcs_routing_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ParcsController, type: :routing do
   describe "routes" do
     it "routes to #index" do
-      expect(get '/parcs/').to route_to 'parcs#index'
+      expect(get '/').to route_to 'home#index'
     end
 
     it "routes to #show" do
