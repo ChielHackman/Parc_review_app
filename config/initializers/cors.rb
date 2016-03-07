e114c72133d322bc815b22d2f75ccdb8d18c8a6b
Chiel Hackman <chielhackman@gmail.com>
2016-03-07 08:14:51 +0100

    Changed url in corsfile

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ class ParcReviewApp::Application
   config.middleware.insert_before 0,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
     allow do
       origins 'localhost:3000',
-      'www.chielhackman.nl'
+      'www.chielhackman.nl/ParcReviewApp'
 
       resource '/cors',
         headers: :any,
