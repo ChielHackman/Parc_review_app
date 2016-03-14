69e6698a28d25827c904975f0357fe62d82bad5f
Chiel Hackman <chielhackman@gmail.com>
2016-03-14 15:38:23 +0100

    Completed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,97 +10,97 @@ Parc.create([
   name: "De Eemhof",
   description: "Eldorado vol avontuur en watersport, met een Mediterraans tintje.",
   city: "Slingerweg 1 Zeewolde",
-  country: "Nederland"
+  country: "nederland"
 },
 {
   name: "Parc Zandvoort",
   description: "Uitwaaien aan het Noordzeestrand of voor even toerist in de grote stad.",
   city: "Vondellaan 60 Zandvoort",
-  country: "Nederland"
+  country: "nederland"
 },
 {
   name: "Het Meerdal",
   description: "Het paradijs waar kinderdromen uitkomen.",
   city: "Laagheideweg 11 America",
-  country: "Nederland"
+  country: "nederland"
 },
 {
   name: "Parc Sandur",
   description: "Rust en ruimte aan een glinsterend meer.",
   city: "Sandurdreef 5 Emmen",
-  country: "Nederland"
+  country: "nederland"
 },
 {
   name: "Het Heijderbos",
   description: "De plek voor jungle-avonturen en spannende activiteiten.",
   city: "Hommersumseweg 43 Heijen",
-  country: "Nederland"
+  country: "nederland"
 },
 {
   name: "Port Zélande",
   description: "Aan het Grevelingenmeer en Noordzeestrand, een frisse duik in vele water- en strandsporten.",
   city: "Port Zélande 2 Ouddorp",
-  country: "Nederland"
+  country: "nederland"
 },
 {
   name: "De Huttenheugte",
   description: "Kinderspeelparadijs in de rustgevende omgeving van het grote meer.",
   city: "Reindersdijk 55 Dalen",
-  country: "Nederland"
+  country: "nederland"
 },
 {
   name: "De Kempervennen",
   description: "Volop in actie, zowel indoor als outdoor met de natuur als speeldomein.",
   city: "Kempervennendreef 8 Westerhoven",
-  country: "Nederland"
+  country: "nederland"
 },
 {
   name: "Limburgse Peel",
   description: "Eindeloos wandelen en fietsen in het Limburgse natuurschoon.",
   city: "Peelheideweg 25 America",
-  country: "Nederland"
+  country: "nederland"
 },
 {
   name: "De Vossemeren",
   description: "Twee indrukwekkende meren, ontelbare watersport- en piratenavonturen.",
   city: "Elzen 145 Lommel",
-  country: "België"
+  country: "belgië"
 },
 {
   name: "Erperheide",
   description: "Het perfecte park om met de kids op ontdekking te gaan, midden in de groene bossen.",
   city: "Erperheidestraat 2 Peer",
-  country: "België"
+  country: "belgië"
 },
 {
   name: "Bispinger Heide",
   description: "De ideale mix van actie en ontspanning voor het hele gezin.",
   city: "Töpinger Straße 69 Bispringen",
-  country: "Duitsland"
+  country: "duitsland"
 },
 {
   name: "Park Bostalsee",
   description: "Actieve vakantie aan het watersportmeer Bostalsee in het glooiende Saarland.",
   city: "Lindenallee 1 Nohfelden",
-  country: "Duitsland"
+  country: "duitsland"
 },
 {
   name: "Park Hochsauerland",
   description: "Wandelen in de zomer en skiën in de winter in het land van 1000 heuvels.",
   city: "Sonnenallee 1 Medebach",
-  country: "Duitsland"
+  country: "duitsland"
 },
 {
   name: "Park Eifel",
   description: "Zorgeloos spelen, omringd door een prachtig vulkaanlandschap.",
   city: "Am Kurberg Gunderath",
-  country: "Duitsland"
+  country: "duitsland"
 },
 {
   name: "Park Nordseeküste",
   description: "Zuivere lucht en oneindige vergezichten over het wad.",
   city: "Nordseeallee 36 Tossens",
-  country: "Duitsland"
+  country: "duitsland"
 }
 ])
 
